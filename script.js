15b97dc3ff051affe527a3c1770b5390a4373446
Surendra <sky958447@gmail.com>
2025-01-29 22:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let res="";
+	for(let i=0;i<s.length;i++){
+		if(s[i]==" "){
+			break;
+		}
+		res+=s[i];
+		
+	}
+	return res;
 }
 
 // Do not change the code below
